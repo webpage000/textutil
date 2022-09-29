e21eb2492148673e022701434a751b9ad9ae30e2
webpage000 <396931074@qq.com>
2022-09-29 21:51:11 +0800

    Initial commit

---
+++ src/main/java/com/myutils/textutil/TextUtilApplication.java
@@ -0,0 +1,13 @@
+package com.myutils.textutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myutils/textutil/TextUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myutils.textutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
